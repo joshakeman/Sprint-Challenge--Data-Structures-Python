f0facaed90b1ad308ade68d96a97ac11b7e45cd1
Josh Akeman <joshakeman@gmail.com>
2019-07-26 13:11:28 -0400

    all ring buffer tests passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,14 +6,23 @@ class RingBuffer:
 
   def append(self, item):
     
-    print(self.storage)
+    # print(self.storage)
     temp = self.get()
+    # temp = self.storage
 
     if len(temp) < 5:
       temp.append(item)
       self.storage = temp + [None]*(5-len(temp))
-    elif len(temp) is 5:
-      print("true")
+    elif len(temp) == 5:
+      print(self.current)
+      temp[self.current] = item
+      # temp.pop()
+      self.storage = temp
+      self.current += 1
+      # if self.current < self.capacity:
+      #   self.current + 1
+      # else:
+      #   self.current = 0
 
     return temp
     # temp = self.get()
@@ -48,21 +57,24 @@ class RingBuffer:
       if self.storage[i] != None:
         # print("true")
         return_list.append(self.storage[i])
-      else:
-        break
+    
     # self.storage = return_list
     # return_list.reverse()
     return return_list
 
 
-thing = RingBuffer(5)
+# thing = RingBuffer(5)
 
-print(thing.append('a'))
-print(thing.append('b'))
-print(thing.append('c'))
-print(thing.append('d'))
-print(thing.append('e'))
-print(thing.append('g'))
+# print(thing.append('a'))
+# print(thing.append('b'))
+# print(thing.append('c'))
+# print(thing.append('d'))
+# print(thing.append('e'))
+# print(thing.append('f'))
+# print(thing.append('g'))
+# print(thing.append('h'))
+# print(thing.append('i'))
+
 
 
 
