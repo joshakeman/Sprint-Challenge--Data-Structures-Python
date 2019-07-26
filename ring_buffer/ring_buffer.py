6604e7c74e9e04be313c00e590f1816ca928a98c
Josh Akeman <joshakeman@gmail.com>
2019-07-26 11:39:13 -0400

    ring buffer tests failing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,39 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    self.storage.insert(0, item)
+    # self.get()
+    # print(self.storage)
+
+    return_list = []
+    for i in range(len(self.storage)):
+      # print (self.storage[i])
+      if self.storage[i] != None:
+        # print("true")
+        return_list.append(self.storage[i])
+      else:
+        break
+    self.storage.pop()
+    return_list.reverse()
+    return return_list
 
   def get(self):
-    pass
+
+    return_list = []
+    for i in range(len(self.storage)):
+      # print (self.storage[i])
+      if self.storage[i] != None:
+        # print("true")
+        return_list.append(self.storage[i])
+      else:
+        break
+    self.storage = return_list
+    return_list.reverse()
+    return return_list
+
+
+# thing = RingBuffer(5)
+
+# print(thing.append('a'))
+# print(thing.append('b'))
+# print(thing.get())
